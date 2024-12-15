c7efdd9d26c300a2ab885f755e1657adce6a6aa7
farimakonare <165604921+farimakonare@users.noreply.github.com>
2024-12-15 00:48:43 +0000

    Fix routing for GitHub Pages

--- my_canteen_frontend/src/App.js
+++ my_canteen_frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Components/Pages/Home';
 import Register from './Components/Pages/Register';
 import Login from './Components/Pages/Login';

8def3a22b66ad7202e570a930d8b694949a126d6
farimakonare <165604921+farimakonare@users.noreply.github.com>
2024-12-16 16:44:17 +0000

    Update GitHub Pages configuration

--- my_canteen_frontend/src/App.js
+++ my_canteen_frontend/src/App.js
@@ -11,7 +11,7 @@ import AdminMenu from './Components/AdminMenu';
 
 function App() {
   return (
-    <Router basename='/farimakonare.github.io/Canteen-Ordering-System-'>
+    <Router basename="/Canteen-Ordering-System-">
       <Routes>
         {/*shares routes*/}
         <Route path="/" element={<Home />} />
